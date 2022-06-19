e3f992c633019ac08cb28c0ac208398bd3823904
bhubhik <bhubhik@gmail.com>
2022-06-19 18:04:49 +1000

    Homepage layout complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Marvel Mania</h1>
+      <div className='page-wrapper'>
+        <header className='header'>
+          <img
+            className='header-logo'
+            src='/images/marvel-mania-logo.png'
+            alt='Logo'
+          />
+        </header>
+        <Body />
+        <footer className='footer'>
+          <h3 className='footer-link'>www.anubhabkarki.com</h3>
+          <h3 className='footer-link'>©2022AnubhabKarki</h3>
+        </footer>
+      </div>
     </div>
   );
 }
